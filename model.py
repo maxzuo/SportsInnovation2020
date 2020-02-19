bdc2f76987f2681d1fbfcc3a1cb9c8b3af29ee42
Max Zuo <max.zuo@gmail.com>
2020-02-19 02:22:17 -0500

    Completely numpy-fied the code as much as seemed feasible. Reduced pipeline processing time from 8-9 seconds to 2-3 seconds.

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn import svm
 import math
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 WEIGHTS_FILE = "cfg/yolov3.weights"
 CONFIG_FILE = "cfg/yolov3.cfg"
@@ -65,16 +66,24 @@ class YOLO(object):
 		# print(boxes)
 		# print(len(boxes))
 
-		indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.1, 0.3)
+		indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.1, 0.3).flatten()
+		# print(indexes)
+		# index_set = set()
+		# for i in indexes: index_set.add(i)
 
 		areas = []
 		new_boxes = []
 
-		for i in range(len(boxes)):
-			if i in indexes:
-				_, _, w, h = boxes[i]
-				areas.append(np.sqrt(w * h))
-				new_boxes.append(boxes[i])
+		# for i in range(len(boxes)):
+		# 	if i in index_set:
+			# 	_, _, w, h = boxes[i]
+			# 	areas.append(np.sqrt(w * h))
+			# 	new_boxes.append(boxes[i])
+
+		for i in indexes:
+			_, _, w, h = boxes[i]
+			areas.append(w * h)
+			new_boxes.append(boxes[i])
 		
 		average_area = np.median(areas)
 		#print(average_area)
@@ -83,8 +92,9 @@ class YOLO(object):
 		boxes = []
 
 		font = cv2.FONT_HERSHEY_PLAIN
+		# consider using filter instead of this method
 		for i in range(len(new_boxes)):
-			if ((areas[i] < 0.2 * average_area) or (areas[i] > 2 * average_area)):
+			if ((areas[i] < 0.2 * average_area) or (areas[i] > 4 * average_area)):
 				continue
 			boxes.append(new_boxes[i])
 
@@ -97,7 +107,7 @@ class YOLO(object):
 			# cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
 
 		cv2.imwrite("result.png", img)
-		return boxes
+		return np.array(boxes)
 
 def mask(img):
 	lower_green = np.array([40,40, 40])
@@ -111,16 +121,15 @@ def mask(img):
 	img = cv2.bitwise_and(img, img, mask=cv2.bitwise_not(mask))
 	return img
 
-def color_extraction(img, k=5, return_labels=False):
+def kmeans(img, k=5, return_labels=False, restarts = 15):
 
 	if len(img.shape) == 3:
-		# print("AHH")
 		img = img.reshape(img.shape[0]*img.shape[1], img.shape[2])
-	# print(img.shape)
+	
 	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
 	flags = cv2.KMEANS_RANDOM_CENTERS
 
-	_, labels, centroids = cv2.kmeans(np.float32(img), k, None, criteria, 10, flags)
+	_, labels, centroids = cv2.kmeans(np.float32(img), k, None, criteria, restarts, flags)
 
 	quantized = np.bincount(labels.flatten())
 
@@ -130,77 +139,78 @@ def color_extraction(img, k=5, return_labels=False):
 
 
 def assign_teams(img, boxes):
+	start = datetime.now()
 	img = mask(img)
+	print("Mask:", datetime.now() - start)
+	start = datetime.now()
 	colors = [None] * len(boxes)
-	minX = None
-	minY = None
+	minX = 10_000
+	minY = 10_000
 	maxX = 0
 	maxY = 0
+
+	
+
 	for i in range(len(boxes)):
 		x, y, w, h = boxes[i]
-		cv2.imwrite("%d.png" % i, img[y:y+h,x:x+w])
-		color = color_extraction(np.float32(img[y:y+h,x:x+w]))
-		color = color.tolist()
-		color.remove(min(color, key=lambda c: np.sum(c)))
-		# color = list(filter(lambda c: np.sum(c) > 30, color))
-		# assert len(color) == 2
-		if not minX:
-			minX = x + w/2
-		if not minY:
-			minY = y + h/2
 		minX = min(minX, x + w/2)
 		minY = min(minY, y + y/2)
 		maxX = max(maxX, x + w/2)
 		maxY = max(maxY, y + h/2)
 
-		boxes[i] = (x + w/2, y + h/2)
-		colors[i] = ((x + w/2, y + h/2), color)
-	
 	maxX -= minX
 	maxY -= minY
 
+	X = np.zeros((len(boxes,)))
+	Y = np.zeros((len(boxes,)))
+	positions = [None] * len(boxes)
+
 	for i in range(len(boxes)):
-		x, y = boxes[i]
+		x, y, w, h = boxes[i]
+		# cv2.imwrite("%d.png" % i, img[y:y+h,x:x+w])
+		color = kmeans(np.float32(cv2.resize(img[y:y+h,x:x+w], None,fx=.1, fy=1.0), restarts=10))
+		
+		# line below for higher accuracy
+		# color = kmeans(np.float32(img[y:y+h,x:x+w]), restarts=10))
+		color = color.tolist()
+		color.remove(min(color, key=lambda c: np.sum(c)))
+		x = x + w/2
+		y = y + h/2
+
 		x = ((x - minX) / maxX - 0.5) * 2
 		y = ((y - minY) / maxY - 0.5) * 2
 
-		boxes[i] = (x, y)
+		X[i] = x
+		Y[i] = y
+		positions[i] = [x, y]
+		colors[i] = np.concatenate([np.asarray(color[0]) / 255, np.asarray([x, y])])
+	
+	
 	# print(colors)
 	# input()
 
-	primary_colors = np.asarray([(np.array(color[0]) / 255).tolist() + [position[0] / 1000, position[1] / 1000] for position, color in colors])
-	assignments = color_extraction(primary_colors, k=2, return_labels=True)
+	primary_colors = np.asarray(colors)
+	assignments = kmeans(primary_colors, k=2, restarts=10, return_labels=True)
 	# print(np.bincount(assignments.flatten()))
 	# input()
 	# res = [None] * len(boxes)
-	return list(zip(boxes, assignments.flatten().tolist())) # for now
+	return X, Y, np.array(positions), assignments.flatten()
 	
-def get_line(teams):
+def get_line(X, labels):
 	# teams should have 0 or 1 labels, not colors, at this stage
-	X = []
-	y = []
-	for i in range(len(teams)):
-		X.append(teams[i][0])
-		y.append(teams[i][1])
 
-	svc = svm.SVC(kernel='linear').fit(np.array(X), np.array(y))
+	svc = svm.SVC(kernel='linear', C=1).fit(X, labels)
 	W = svc.coef_[0]
 	I = svc.intercept_
 	slope = -W[0]/W[1]
-	#b = -I[0]/W[1]
+	b = -I[0]/W[1]
 	return slope, I
 
-def transform_coordinates(teams, slope, intercept):
+def transform_coordinates(positions, slope, intercept):
 	newCoords = []
 	theta = math.atan(slope)
-	for i in range(len(teams)):
-		x, y = teams[i][0]
-		newX = math.cos(-theta) * x - math.sin(theta) * y
-		newY = math.sin(theta) * x + math.cos(-theta) * y
-		#newX = (1 / math.sqrt(1 + slope**2)) * x + (-1 / math.sqrt(1 + (1/slope)**2)) * y
-		#newY = (slope / math.sqrt(1 + slope**2)) * x + (slope / mat.sqrt(1 + (1/slope)**2)) * y
-		newCoords.append(((newX, newY), teams[i][1]))
-	return newCoords
+	transformation = np.array([[np.cos(-theta), -np.sin(theta)], [np.sin(theta), np.cos(-theta)]])
+	return transformation.dot(positions.T), intercept * math.cos(-theta)
 
 
 if __name__ == "__main__":
@@ -208,43 +218,50 @@ if __name__ == "__main__":
 
 	yolo_net = YOLO()
 	
-	
+	start = datetime.now()	
 	boxes = yolo_net.predict(img)
+	print("YOLO finished", datetime.now() - start)
+	start = datetime.now()
 
-	teams = assign_teams(img, boxes)
-	slope, intercept = get_line(teams)
+	X, Y, positions, labels = assign_teams(img, boxes)
+	print("Assigned teams", datetime.now() - start)
+	start = datetime.now()
+
+	slope, intercept = get_line(positions, labels)
+	rotated, intercept = transform_coordinates(positions, slope, intercept)
 	print(intercept)
-	rotated, intercept = transform_coordinates(teams, slope, intercept)
-	print(intercept)
-
-
-	# plt.title("KNN - " + data + ": Euclidean Distance")
+	print("rotated", datetime.now() - start)
+	start = datetime.now()
 	
-	X = []
-	Y = []
-	labels = []
-	for (x, y), label in rotated:
-		X.append(x)
-		Y.append(y)
-		labels.append(label)
 	
 	plt.scatter(X, Y, c=labels)
+	# plt.plot(X, [intercept + slope * i for i in X], "b-", label="Line of Scrimmage")
 	# plt.show()
 	# ax.plot(list(range(1, maxK + 1)), kNN_euc_loss, "g-", label="Testing Loss")
 	
+	plt.savefig("not_rotated.png")
+	plt.clf()
+
+	X = rotated[:][0]
+	Y = rotated[:][1]
+	# print(X.reshape(-1, 1).shape, colors.T[:][:3].T.shape)
+	# assignments = kmeans(np.concatenate((colors.T[:][:3].T, Y.reshape(-1, 1) * 1.5), axis=1), k=2, restarts=20, return_labels=True)
+	# print(assignments.flatten() - labels)
+	plt.scatter(X, Y, c=labels.flatten())
+	plt.plot(X, np.ones(X.shape) * intercept, "b-", label="Line of Scrimmage")
 	plt.savefig("rotated.png")
 	plt.clf()
 
 	
 
 	# print(teams[0])
-	font = cv2.FONT_HERSHEY_PLAIN
-	color = yolo_net.colors[0]
-	for position, assignment in teams:
-		x, y = position
-		label = str(assignment)
-		cv2.putText(img, label, (int(x), int(y) + 30), font, 3, color, 3)
-	cv2.imwrite("labels.png", img)
+	# font = cv2.FONT_HERSHEY_PLAIN
+	# color = yolo_net.colors[0]
+	# for position, assignment in teams:
+	# 	x, y = position
+	# 	label = str(assignment)
+	# 	cv2.putText(img, label, (int(x), int(y) + 30), font, 3, color, 3)
+	# cv2.imwrite("labels.png", img)
 
 	
 
@@ -308,9 +325,9 @@ if __name__ == "__main__":
 	# for i in range(1):
 	# 	primary_colors = np.asarray([(np.array(color[i]) / 255).tolist() +[position[0] / 1000, position[1] / 1000] for position, color in teams])
 	# 	# print(primary_colors.shape)
-	# 	assignments = color_extraction(primary_colors, k=2, return_labels=True)
+	# 	assignments = kmeans(primary_colors, k=2, return_labels=True)
 	# 	print(assignments)
-		# print(np.bincount(color_extraction(primary_colors, k=2, return_labels=True).flatten()))
+		# print(np.bincount(kmeans(primary_colors, k=2, return_labels=True).flatten()))
 		# summation = np.sum(primary_colors, axis=1)
 		# # print(summation)
 		# primary_colors = primary_colors[summation.argsort()]
